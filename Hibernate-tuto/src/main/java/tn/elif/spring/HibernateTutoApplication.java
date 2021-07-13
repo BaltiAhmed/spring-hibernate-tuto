7273490788273b20a365e437d5ac482d670e0b34
BaltiAhmed <ahmedbalti06@gmail.com>
2021-07-13 12:26:59 +0100

    ahmed

---
+++ Hibernate-tuto/src/main/java/tn/elif/spring/HibernateTutoApplication.java
@@ -0,0 +1,13 @@
+package tn.elif.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateTutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateTutoApplication.class, args);
+	}
+
+}
