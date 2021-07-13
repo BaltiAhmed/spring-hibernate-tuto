125bf64523703c09fe5eb5d64b6b978b2cd674c6
BaltiAhmed <ahmedbalti06@gmail.com>
2021-07-13 12:43:26 +0100

    aaa

--- Hibernate-tuto/src/main/java/tn/elif/spring/hibernate/demo/entity/Student.java
+++ Hibernate-tuto/src/main/java/tn/elif/spring/hibernate/demo/entity/Student.java
@@ -13,7 +13,7 @@ public class Student {
 	@Column(name="id")
 	private int id;
 	
-	@Column(name="first_name")
+	@Column(name="first_namee")
 	private String firstName;
 	
 	@Column(name="last_name")
